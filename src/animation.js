a23694412d7d3bbecda29c16bae3fc2f77a97860
oley-projects <oley.for.git@gmail.com>
2021-11-19 20:26:02 +0200

    about section transition

--- src/animation.js
+++ src/animation.js
@@ -8,13 +8,56 @@ export const pageAnimation = {
     y: 0,
     transition: {
       duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.5,
     },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 1,
+      duration: 0.5,
+      ease: "easeOut",
     },
-  }
+  },
+};
+export const titleAnim = {
+  hidden: {
+    opacity: 0,
+    x: 50,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 2,
+      ease: "easeOut",
+    },
+  },
+}
+export const fade = {
+  hidden: {
+    opacity: 0,
+  },
+  show : {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
+};
+export const imageAnim = {
+  hidden: {
+    scale: 1.4,
+    opacity: 0,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import home1 from "../img/home1.png";
 import { About, Descriptiom, Image, Hide } from "../styles";
 // Framer motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, imageAnim } from "../animation";
 
 const AboutSection = () => {
   return(
@@ -9,23 +10,23 @@ const AboutSection = () => {
       <Descriptiom>
         <motion.div className="title">
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>your <span>dreams</span></motion.h2>
+            <motion.h2 variants={titleAnim}>your <span>dreams</span></motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>come true</motion.h2>
+            <motion.h2 variants={titleAnim}>come true</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photo or video ideas that you have. 
           We are professionals with amazing skills to help you
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Descriptiom>
       <Image>
-        <img src={home1} alt="home" />
+        <motion.img variants={imageAnim} src={home1} alt="home" />
       </Image>
     </About>
   );
