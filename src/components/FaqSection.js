4aaecc1a34c56988df6823295c12310acdec47cf
oley-projects <oley.for.git@gmail.com>
2021-11-21 22:38:07 +0200

    faq section transition

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,71 +1,69 @@
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
-    return(
-        <Faq>
-            <h2>Any Questions? <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do I start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Scheldule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-        </Faq>
-    );
+  return (
+    <Faq>
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title="How do I start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Scheldule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+          </div>
+        </Toggle>
+        <Toggle title="Different payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+          </div>
+        </Toggle>
+        <Toggle title="What products do you offer">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </Faq>
+  );
 };
 
 const Faq = styled(About)`
+  display: block;
+  span {
     display: block;
-    span {
-        display: block;
+  }
+  h2 {
+    padding-bottom: 2px;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background-color: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0;
+    p {
+      padding: 1rem 0;
     }
-    h2 {
-        padding-bottom: 2px;
-        font-weight: lighter;
-    }
-    .faq-line {
-        background-color: #ccc;
-        height: 0.2rem;
-        margin: 2rem 0;
-        width: 100%;
-    }
-    .question {
-        padding: 3rem 0;
-        cursor: pointer;
-    }
-    .answer {
-        padding: 2rem 0;
-        p {
-            padding: 1rem 0;
-        }
-    }
+  }
 `;
 
 export default FaqSection;
